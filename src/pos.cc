0a4f97f6753e481a0cec46a4febbcce16e14ed5e
Tran Sang <tranvansangk41@gmail.com>
2017-06-27 21:32:06 +0900

    adapt to new swiseph version

--- src/pos.cc
+++ src/pos.cc
@@ -622,9 +622,7 @@ NAN_METHOD(node_swe_get_ayanamsa_name) {
 		Nan::ThrowTypeError ("Wrong type of arguments");
 	};
 
-	char * val;
-
-	val = ::swe_get_ayanamsa_name (
+	const char * val = ::swe_get_ayanamsa_name (
 		(int)info [0]->NumberValue ()
 	);
 
